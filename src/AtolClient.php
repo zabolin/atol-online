0bceab362337377835392abb8f8c566a549aa524
zabolin <zabolin@ya.ru>
2024-08-16 17:06:55 +0200

    Добавлена возможность задания хоста в конструкторах клиентов

--- src/AtolClient.php
+++ src/AtolClient.php
@@ -18,207 +18,210 @@ use Psr\SimpleCache\CacheInterface;
 
 class AtolClient
 {
-  /** @var string */
-  private $host = 'https://online.atol.ru';
+    /** @var string */
+    private $host = 'https://online.atol.ru';
 
-  /** @var string */
-  private $apiVersion = 'v4';
+    /** @var string */
+    private $apiVersion = 'v4';
 
-  /** @var string */
-  private $token;
+    /** @var string */
+    private $token;
 
-  /** @var string */
-  private $groupCode;
+    /** @var string */
+    private $groupCode;
 
-  /** @var LoggerInterface */
-  private $logger;
+    /** @var LoggerInterface */
+    private $logger;
 
-  /** @var CacheInterface */
-  private $cache;
+    /** @var CacheInterface */
+    private $cache;
 
-  /**
-   * @param string $login Shop ID
-   * @param string $password Secret key
-   * @param string $groupCode Group code
-   * @param CacheInterface $cache
-   * @param LoggerInterface $logger PSR Logger
-   */
-  public function __construct(string $login, string $password, string $groupCode, CacheInterface $cache, LoggerInterface $logger = null)
-  {
-    $this->groupCode = $groupCode;
+    /**
+     * @param string $login Shop ID
+     * @param string $password Secret key
+     * @param string $groupCode Group code
+     * @param CacheInterface $cache
+     * @param LoggerInterface $logger PSR Logger
+     */
+    public function __construct(string $login, string $password, string $groupCode, CacheInterface $cache, LoggerInterface $logger = null, string $host = null)
+    {
+        $this->groupCode = $groupCode;
 
-    $this->logger = $logger;
-    $this->cache = $cache;
-    $this->token = $this->getToken($login, $password);
-  }
-
-  /**
-   * @param string $value
-   *
-   * @return $this
-   */
-  public function setHost($value): self
-  {
-    $this->host = $value;
-
-    return $this;
-  }
-
-  /**
-   * Отпаравляет запрос на регистрацию документа в АТОЛ Онлайн.
-   *
-   * Возвращает уникальный идентификатор, присвоенный данному документу.
-   *
-   * @param Request $request Запрос
-   *
-   * @return string Уникальный идентификатор, присвоенный данному документу
-   */
-  public function send(Request $request): string
-  {
-    $response = $this->sendRequest($request->getOperation(), $request->toArray());
-
-    $error = $response['error'];
-    if (null !== $error) {
-      $this->log(LogLevel::WARNING, 'error: {error} {response}', [
-        'error' => $error['text'],
-        'response' => $response,
-      ]);
-      throw new ClientException($error['error_id'].' - '.$error['text']);
+        $this->logger = $logger;
+        $this->cache = $cache;
+        if (!is_null($host)) {
+            $this->host = $host;
+        }
+        $this->token = $this->getToken($login, $password);
     }
 
-    return $response['uuid'];
-  }
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setHost($value): self
+    {
+        $this->host = $value;
 
-  /**
-   * Получает статуса обработки документа из АТОЛ Онлайн.
-   *
-   * Возвращает обработанный ответ.
-   *
-   * @param string $uuid Уникальный идентификатор, присвоенный документу после выполнения запроса на регистрацию
-   *
-   * @return Report
-   */
-  public function getReport(string $uuid): Report
-  {
-    $path = sprintf('report/%s', $uuid);
-    $response = $this->sendRequest($path);
-
-    return Report::fromArray($response);
-  }
-
-  /**
-   * @param string $path
-   * @param array  $data
-   *
-   * @return array|null
-   */
-  private function sendRequest(string $path, ?array $data = null): array
-  {
-    $path = sprintf('possystem/%s/%s/%s', $this->apiVersion, $this->groupCode, $path);
-
-    return $this->sendRawRequest($path, $data);
-  }
-
-  private function getToken(string $login, string $password): string
-  {
-    $CACHE_KEY = 'atol.token' . $login;
-    if($this->cache->has($CACHE_KEY)) {
-      return (string)$this->cache->get($CACHE_KEY);
+        return $this;
     }
 
-    $data = [
-      'login' => $login,
-      'pass' => $password,
-    ];
+    /**
+     * Отпаравляет запрос на регистрацию документа в АТОЛ Онлайн.
+     *
+     * Возвращает уникальный идентификатор, присвоенный данному документу.
+     *
+     * @param Request $request Запрос
+     *
+     * @return string Уникальный идентификатор, присвоенный данному документу
+     */
+    public function send(Request $request): string
+    {
+        $response = $this->sendRequest($request->getOperation(), $request->toArray());
 
-    $path = sprintf('possystem/%s/getToken', $this->apiVersion);
+        $error = $response['error'];
+        if (null !== $error) {
+            $this->log(LogLevel::WARNING, 'error: {error} {response}', [
+                'error' => $error['text'],
+                'response' => $response,
+            ]);
+            throw new ClientException($error['error_id'] . ' - ' . $error['text']);
+        }
 
-    $response = $this->sendRawRequest($path, $data);
-
-    $error = $response['error'];
-    if (null !== $error) {
-      $this->log(LogLevel::WARNING, 'error: {error} {response}', [
-        'error' => $error['text'],
-        'response' => $response,
-      ]);
-      throw new ClientException($error['error_id'].' - '.$error['text']);
+        return $response['uuid'];
     }
 
-    $token = $response['token'];
-    $this->cache->set($CACHE_KEY, $token, 3600 * 24);
+    /**
+     * Получает статуса обработки документа из АТОЛ Онлайн.
+     *
+     * Возвращает обработанный ответ.
+     *
+     * @param string $uuid Уникальный идентификатор, присвоенный документу после выполнения запроса на регистрацию
+     *
+     * @return Report
+     */
+    public function getReport(string $uuid): Report
+    {
+        $path = sprintf('report/%s', $uuid);
+        $response = $this->sendRequest($path);
 
-    return $token;
-  }
-
-  /**
-   * @param string     $path
-   * @param array|null $data
-   *
-   * @return array
-   */
-  private function sendRawRequest(string $path, $data = null): array
-  {
-    if (null === $data) {
-      $method = 'GET';
-    } elseif (is_array($data)) {
-      $method = 'POST';
-      $data = json_encode($data);
-    } else {
-      throw new InvalidArgumentException('Unexpected type of $data, excepts array or null');
+        return Report::fromArray($response);
     }
 
-    $url = sprintf('%s/%s', $this->host, $path);
+    /**
+     * @param string $path
+     * @param array $data
+     *
+     * @return array|null
+     */
+    private function sendRequest(string $path, ?array $data = null): array
+    {
+        $path = sprintf('possystem/%s/%s/%s', $this->apiVersion, $this->groupCode, $path);
 
-    $headers = [
-      'Accept: application/json; charset=utf-8',
-      'Token: '.$this->token,
-    ];
-
-    if ('POST' == $method) {
-      $headers[] = 'Content-Type: application/json';
+        return $this->sendRawRequest($path, $data);
     }
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    if ('POST' == $method) {
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    private function getToken(string $login, string $password): string
+    {
+        $CACHE_KEY = 'atol.token' . $login;
+        if ($this->cache->has($CACHE_KEY)) {
+            return (string)$this->cache->get($CACHE_KEY);
+        }
+
+        $data = [
+            'login' => $login,
+            'pass' => $password,
+        ];
+
+        $path = sprintf('possystem/%s/getToken', $this->apiVersion);
+
+        $response = $this->sendRawRequest($path, $data);
+
+        $error = $response['error'];
+        if (null !== $error) {
+            $this->log(LogLevel::WARNING, 'error: {error} {response}', [
+                'error' => $error['text'],
+                'response' => $response,
+            ]);
+            throw new ClientException($error['error_id'] . ' - ' . $error['text']);
+        }
+
+        $token = $response['token'];
+        $this->cache->set($CACHE_KEY, $token, 3600 * 24);
+
+        return $token;
     }
 
-    $response = curl_exec($ch);
+    /**
+     * @param string $path
+     * @param array|null $data
+     *
+     * @return array
+     */
+    private function sendRawRequest(string $path, $data = null): array
+    {
+        if (null === $data) {
+            $method = 'GET';
+        } elseif (is_array($data)) {
+            $method = 'POST';
+            $data = json_encode($data);
+        } else {
+            throw new InvalidArgumentException('Unexpected type of $data, excepts array or null');
+        }
 
-    $this->log(LogLevel::DEBUG, 'request: url={url} headers={headers} data={data}', [
-      'url' => $url,
-      'headers' => $headers,
-      'data' => $data,
-    ]);
+        $url = sprintf('%s/%s', $this->host, $path);
 
-    $error = null;
-    if (false === $response) {
-      $error = curl_error($ch);
+        $headers = [
+            'Accept: application/json; charset=utf-8',
+            'Token: ' . $this->token,
+        ];
+
+        if ('POST' == $method) {
+            $headers[] = 'Content-Type: application/json';
+        }
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        if ('POST' == $method) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
+        $response = curl_exec($ch);
+
+        $this->log(LogLevel::DEBUG, 'request: url={url} headers={headers} data={data}', [
+            'url' => $url,
+            'headers' => $headers,
+            'data' => $data,
+        ]);
+
+        $error = null;
+        if (false === $response) {
+            $error = curl_error($ch);
+        }
+
+        curl_close($ch);
+        if (null !== $error) {
+            $this->log(LogLevel::WARNING, 'error: {error} {response}', [
+                'error' => $error,
+                'response' => $response,
+            ]);
+            throw new ClientException($error);
+        }
+
+        $this->log(LogLevel::DEBUG, 'response: {response}', ['response' => $response]);
+
+        return json_decode($response, true);
     }
 
-    curl_close($ch);
-    if (null !== $error) {
-      $this->log(LogLevel::WARNING, 'error: {error} {response}', [
-        'error' => $error,
-        'response' => $response,
-      ]);
-      throw new ClientException($error);
+    private function log($level, $message, $context)
+    {
+        if (null !== $this->logger) {
+            $message = sprintf('Atol Online %s', $message);
+            $this->logger->log($level, $message, $context);
+        }
     }
-
-    $this->log(LogLevel::DEBUG, 'response: {response}', ['response' => $response]);
-
-    return json_decode($response, true);
-  }
-
-  private function log($level, $message, $context)
-  {
-    if (null !== $this->logger) {
-      $message = sprintf('Atol Online %s', $message);
-      $this->logger->log($level, $message, $context);
-    }
-  }
 }
